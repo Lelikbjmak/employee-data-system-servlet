6ee1667546b2e728773b03dcb4b7cdac51999c3b
Lelikbjmak <denissolovey990@gmail.com>
2023-04-06 23:50:10 +0300

    refactor: Change all magic Strings to constants.

--- src/main/java/com/innowise/employeesystem/mapper/UserMapper.java
+++ src/main/java/com/innowise/employeesystem/mapper/UserMapper.java
@@ -3,19 +3,21 @@ package com.innowise.employeesystem.mapper;
 import com.innowise.employeesystem.dto.RegistrationUserDto;
 import com.innowise.employeesystem.dto.UserDto;
 import com.innowise.employeesystem.entity.User;
+import com.innowise.employeesystem.util.DtoConstant;
+import com.innowise.employeesystem.util.EntityConstant;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(uses = RoleMapper.class)
 public interface UserMapper {
 
-    @Mapping(source = "roles", target = "roleSet")
+    @Mapping(source = DtoConstant.UserDto.ROLE_SET_FILED_NAME, target = EntityConstant.User.ROLE_SET_FIELD_NAME)
     User mapToEntity(UserDto userDto);
 
-    @Mapping(source = "roleSet", target = "roles")
+    @Mapping(source = EntityConstant.User.ROLE_SET_FIELD_NAME, target = DtoConstant.UserDto.ROLE_SET_FILED_NAME)
     UserDto mapToDto(User user);
 
-    @Mapping(source = "roles", target = "roleSet")
+    @Mapping(source = DtoConstant.UserDto.ROLE_SET_FILED_NAME, target = EntityConstant.User.ROLE_SET_FIELD_NAME)
     User mapToEntity(RegistrationUserDto registrationUserDto);
 
 }
