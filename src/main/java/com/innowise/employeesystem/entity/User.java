2cc229da5a03c02adc4125e9b05c349bc3f85774
Lelikbjmak <denissolovey990@gmail.com>
2023-04-10 23:20:47 +0300

    feature: Implement Employee & RoleSet lazy loading.

--- src/main/java/com/innowise/employeesystem/entity/User.java
+++ src/main/java/com/innowise/employeesystem/entity/User.java
@@ -1,5 +1,9 @@
 package com.innowise.employeesystem.entity;
 
+import com.innowise.employeesystem.dao.EmployeeDao;
+import com.innowise.employeesystem.dao.RoleDao;
+import com.innowise.employeesystem.daoimpl.EmployeeDaoImpl;
+import com.innowise.employeesystem.daoimpl.RoleDaoImpl;
 import lombok.*;
 
 import java.util.Set;
@@ -7,10 +11,17 @@ import java.util.Set;
 @Setter
 @Getter
 @Builder
+@ToString
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
+
+    @EqualsAndHashCode.Exclude
+    private static final RoleDao roleDao = RoleDaoImpl.getInstance();
+
+    @EqualsAndHashCode.Exclude
+    private static final EmployeeDao employeeDao = EmployeeDaoImpl.getInstance();
 
     @EqualsAndHashCode.Exclude
     private long id;
@@ -25,4 +36,21 @@ public class User {
 
     private Set<Role> roleSet;
 
+    public Set<Role> getRoleSet() {
+
+        if (roleSet == null)
+            this.roleSet = roleDao.getUserRoles(id);
+
+
+        return roleSet;
+    }
+
+    public Employee getEmployee() {
+
+        if (employee == null) {
+            this.employee = employeeDao.findById(id).orElse(null);
+        }
+
+        return employee;
+    }
 }
