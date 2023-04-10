1ab1604d9fa72d8ac40787e40782be4f346b8cef
Lelikbjmak <denissolovey990@gmail.com>
2023-04-10 23:20:30 +0300

    feature: Implement User lazy loading.

--- src/main/java/com/innowise/employeesystem/entity/Employee.java
+++ src/main/java/com/innowise/employeesystem/entity/Employee.java
@@ -1,16 +1,27 @@
 package com.innowise.employeesystem.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.innowise.employeesystem.dao.UserDao;
+import com.innowise.employeesystem.daoimpl.UserDaoImpl;
 import lombok.*;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Setter
 @Getter
 @Builder
+@ToString
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 public class Employee {
+
+    @EqualsAndHashCode.Exclude
+    private static final UserDao USER_DAO = UserDaoImpl.getInstance();
 
     @EqualsAndHashCode.Exclude
     private long id;
@@ -21,7 +32,18 @@ public class Employee {
 
     private String middleName;
 
-    private LocalDateTime hireDate;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate hireDate;
 
     private User user;
+
+    public User getUser() {
+
+        if (user == null)
+            user = USER_DAO.findById(id).orElse(null);
+
+        return user;
+    }
 }
