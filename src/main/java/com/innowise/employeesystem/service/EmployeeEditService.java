5e9cecb9460b2cf7a56d16822d4dce6977cbe685
Lelikbjmak <denissolovey990@gmail.com>
2023-04-11 23:43:36 +0300

    feature: Implement EmployeeEditService.

---
+++ src/main/java/com/innowise/employeesystem/service/EmployeeEditService.java
@@ -0,0 +1,9 @@
+package com.innowise.employeesystem.service;
+
+import com.innowise.employeesystem.dto.EmployeeDto;
+
+public interface EmployeeEditService {
+
+    EmployeeDto edit(EmployeeDto employee);
+
+}
---
+++ src/main/java/com/innowise/employeesystem/serviceimpl/EmployeeEditServiceImpl.java
@@ -0,0 +1,50 @@
+package com.innowise.employeesystem.serviceimpl;
+
+import com.innowise.employeesystem.dto.EmployeeDto;
+import com.innowise.employeesystem.dto.UserDto;
+import com.innowise.employeesystem.entity.Employee;
+import com.innowise.employeesystem.mapper.EmployeeMapper;
+import com.innowise.employeesystem.service.EmployeeEditService;
+import com.innowise.employeesystem.service.EmployeeService;
+import com.innowise.employeesystem.service.UserService;
+import org.mapstruct.factory.Mappers;
+
+public class EmployeeEditServiceImpl implements EmployeeEditService {
+
+    private static EmployeeEditServiceImpl instance;
+
+    private final EmployeeService employeeService;
+
+    private final UserService userService;
+
+    private final EmployeeMapper employeeMapper;
+
+    private EmployeeEditServiceImpl() {
+        this.employeeService = EmployeeServiceImpl.getInstance();
+        this.userService = UserServiceImpl.getInstance();
+        this.employeeMapper = Mappers.getMapper(EmployeeMapper.class);
+    }
+
+    public static EmployeeEditServiceImpl getInstance() {
+        if (instance == null)
+            instance = new EmployeeEditServiceImpl();
+
+        return instance;
+    }
+
+    @Override
+    public EmployeeDto edit(EmployeeDto employeeDto) {
+        if (employeeDto == null)
+            return null;
+
+        Employee employee = employeeMapper.mapToEntity(employeeDto);
+
+        EmployeeDto editedEmployee = employeeService.save(employee);
+        if (employee.getUser() != null) {
+            UserDto editedUser = userService.save(employee.getUser());
+            editedEmployee.setUserDto(editedUser);
+        }
+
+        return editedEmployee;
+    }
+}
