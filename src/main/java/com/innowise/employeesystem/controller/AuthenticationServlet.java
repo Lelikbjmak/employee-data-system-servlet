be15ce18880700d68ff7d8fe16cacc435180ea18
Lelikbjmak <denissolovey990@gmail.com>
2023-04-13 23:32:54 +0300

    feature: Implement AuthenticationServlet.

---
+++ src/main/java/com/innowise/employeesystem/controller/AuthenticationServlet.java
@@ -0,0 +1,28 @@
+package com.innowise.employeesystem.controller;
+
+import com.innowise.employeesystem.service.RequestStrategyService;
+import com.innowise.employeesystem.serviceimpl.RequestStrategyServiceImpl;
+import com.innowise.employeesystem.util.ApiConstant;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = ApiConstant.Routes.AUTHENTICATION_ROUTE)
+public class AuthenticationServlet extends HttpServlet {
+
+    private final RequestStrategyService requestStrategyService;
+
+
+    public AuthenticationServlet() {
+        this.requestStrategyService = RequestStrategyServiceImpl.getInstance();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        requestStrategyService.process(request, response);
+    }
+}
