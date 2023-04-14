0d857be50473ccff88eba4868046246a8d710a37
Lelikbjmak <denissolovey990@gmail.com>
2023-04-15 00:42:51 +0300

    feature: Implement MessageConstant with messages.

---
+++ src/main/java/com/innowise/employeesystem/util/MessageConstant.java
@@ -0,0 +1,14 @@
+package com.innowise.employeesystem.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class MessageConstant {
+
+    public static final String ROLE_IS_NOT_FOUND_EXCEPTION_MESSAGE = "Role is not found.";
+    public static final String LOGOUT_MESSAGE = "Successfully logout.";
+    public static final String LOGIN_MESSAGE = "Successfully sign in.";
+    public static final String USER_NOT_FOUND = "User not found.";
+    public static final String INCORRECT_PASSWORD = "Incorrect password.";
+    public static final String DELETE_EMPLOYEE_MESSAGE = "Successfully deleted employee.";
+}
