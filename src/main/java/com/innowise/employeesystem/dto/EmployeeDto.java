f1cf65220ad2b338a28b4d6db61069107e6c0f93
Lelikbjmak <denissolovey990@gmail.com>
2023-04-15 00:34:55 +0300

    feature: Add id filed. Exclude null fields from serialization and deserialization.

--- src/main/java/com/innowise/employeesystem/dto/EmployeeDto.java
+++ src/main/java/com/innowise/employeesystem/dto/EmployeeDto.java
@@ -1,22 +1,28 @@
 package com.innowise.employeesystem.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.innowise.employeesystem.util.EntityConstant;
 import lombok.*;
 
 import java.time.LocalDate;
 
+@Setter
 @Getter
 @Builder
 @ToString
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class EmployeeDto {
+
+    private long id;
 
     private String firstName;
 
@@ -29,6 +35,6 @@ public class EmployeeDto {
     @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate hireDate;
 
-    @JsonProperty(value = "user")
+    @JsonProperty(value = EntityConstant.User.USER_ENTITY_NAME)
     private UserDto userDto;
 }
