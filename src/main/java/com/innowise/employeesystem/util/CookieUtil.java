70b3ee78abcd61a117488688dc67dda1104ff498
Lelikbjmak <denissolovey990@gmail.com>
2023-04-15 00:41:39 +0300

    feature: Add CookieUtil to extract certain cookie from request.

---
+++ src/main/java/com/innowise/employeesystem/util/CookieUtil.java
@@ -0,0 +1,18 @@
+package com.innowise.employeesystem.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.experimental.UtilityClass;
+
+import java.util.Arrays;
+
+@UtilityClass
+public class CookieUtil {
+
+    public static Cookie retrieveCookie(HttpServletRequest request, String cookieName) {
+
+        return Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals(cookieName))
+                .findFirst().orElse(null);
+    }
+}
