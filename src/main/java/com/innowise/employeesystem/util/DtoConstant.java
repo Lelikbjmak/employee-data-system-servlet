6497ddac145839815b58bda49858294ca5d51149
Lelikbjmak <denissolovey990@gmail.com>
2023-04-15 00:42:15 +0300

    feature: Implement DtoConstant for mappers.

---
+++ src/main/java/com/innowise/employeesystem/util/DtoConstant.java
@@ -0,0 +1,13 @@
+package com.innowise.employeesystem.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class DtoConstant {
+
+    @UtilityClass
+    public static class UserDto {
+        public static final String USER_DTO_NAME = "userDto";
+        public static final String ROLE_SET_FILED_NAME = "roles";
+    }
+}
