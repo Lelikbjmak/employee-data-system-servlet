da5531e97b64657b00a63efceb6828bf8ab4f56d
Lelikbjmak <denissolovey990@gmail.com>
2023-04-17 00:20:42 +0300

    fix: Implement DaoException.

---
+++ src/main/java/com/innowise/employeesystem/exception/DaoException.java
@@ -0,0 +1,17 @@
+package com.innowise.employeesystem.exception;
+
+public class DaoException extends Exception {
+
+    public DaoException() {
+        super();
+    }
+
+    public DaoException(Throwable cause) {
+        super(cause);
+    }
+
+    public DaoException(String message) {
+        super(message);
+    }
+
+}
