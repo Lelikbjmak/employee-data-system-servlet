3fbe5c87651dc85d1983d9adfa8d02b70e367dce
Lelikbjmak <denissolovey990@gmail.com>
2023-04-17 00:07:02 +0300

    refactor: Obtain password-encoder properties form .yaml instead of String.

--- src/main/java/com/innowise/employeesystem/security/Argon2PasswordEncoder.java
+++ src/main/java/com/innowise/employeesystem/security/Argon2PasswordEncoder.java
@@ -1,5 +1,6 @@
 package com.innowise.employeesystem.security;
 
+import com.innowise.employeesystem.util.PropertyUtil;
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 
@@ -11,8 +12,22 @@ public class Argon2PasswordEncoder {
 
     private final Argon2 argon2;
 
+    private static final int SALT_LENGTH;
+    private static final int HASH_LENGTH;
+    private static final int ITERATIONS;
+    private static final int MEMORY;
+    private static final int PARALLELISM;
+
+    static {
+        SALT_LENGTH = (int) PropertyUtil.getProperty(SALT_LENGTH_PROPERTY);
+        HASH_LENGTH = (int) PropertyUtil.getProperty(HASH_LENGTH_PROPERTY);
+        ITERATIONS = (int) PropertyUtil.getProperty(ITERATIONS_PROPERTY);
+        MEMORY = (int) PropertyUtil.getProperty(MEMORY_PROPERTY);
+        PARALLELISM = (int) PropertyUtil.getProperty(PARALLELISM_PROPERTY);
+    }
+
     private Argon2PasswordEncoder() {
-        argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2i, DEFAULT_SALT_LENGTH, DEFAULT_HASH_LENGTH);
+        argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2i, SALT_LENGTH, HASH_LENGTH);
     }
 
     public static Argon2PasswordEncoder getInstance() {
